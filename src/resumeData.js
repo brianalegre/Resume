975e97231df6aefd00a27a17559d90d5aedf7233
Brian Alegre <brialegre@yahoo.com>
2022-09-13 14:17:47 -0700

    Updated Projects in resumeData.js

--- src/resumeData.js
+++ src/resumeData.js
@@ -18,6 +18,38 @@ const resumeData = [
                 "githubUrl": "https://github.com/brianalegre/OldEgg",
                 "primaryLanguage": "Javascript"
             },
+            {
+                "name": "The Movie Library",
+                "displayName": "The Movie Library",
+                "summary": "The Movie Library is front-end application where users can search and find information on the latest and/or popular movies",
+                "website": "https://brianalegre.github.io/Movie-Library/",
+                "githubUrl": "https://github.com/brianalegre/Movie-Library",
+                "primaryLanguage": "Javascript"
+            },
+            {
+                "name": "Tech Blog",
+                "displayName": "Tech Blog",
+                "summary": "Tech Blog is a full-stack application, where uesr can post their thoughts, and comment on other users' posts",
+                "website": "https://alegre-techblog.herokuapp.com/",
+                "githubUrl": "https://github.com/brianalegre/14-MVC-Techblog",
+                "primaryLanguage": "Javascript"
+            },
+            {
+                "name": "Weather Dashboard",
+                "displayName": "Weather Dashboard",
+                "summary": "Weather Dashboard is a front-end application, utilizing a third party API, where users can search any city's weather",
+                "website": "https://brianalegre.github.io/06-Server-Side-APIs-Homework/",
+                "githubUrl": "https://github.com/brianalegre/06-Server-Side-APIs-Homework",
+                "primaryLanguage": "Javascript"
+            },
+            {
+                "name": "Pokémon Quiz",
+                "displayName": "Pokémon Quiz",
+                "summary": "Pokémon Quiz is a front-end application where users guess the Pokémon's name, and earn points while doing so",
+                "website": "https://brianalegre.github.io/04-Web-APIs-Homework/",
+                "githubUrl": "https://github.com/brianalegre/04-Web-APIs-Homework",
+                "primaryLanguage": "Javascript"
+            },
         ]
     },
     {
