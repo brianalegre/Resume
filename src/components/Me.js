a5f02e59c4b152b300ad0328111f13548e4acfdf
Brian Alegre <brialegre@yahoo.com>
2022-09-19 21:43:01 -0700

    Added icons to Me Component

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,6 +1,11 @@
 // Import React
 import React from 'react';
 
+// Import React Icons
+import { BsGithub } from 'react-icons/bs'
+import { AiFillLinkedin } from 'react-icons/ai'
+import { CgProfile } from 'react-icons/cg'
+
 
 
 // Me component
@@ -24,9 +29,9 @@ function Me(resumeData) {
             <p className="text-sm">{me.location.city}</p>
             <p className="text-sm">{me.location.region}, {me.location.countryCode}</p>
             <div className="space-x-3 py-3 flex items-center">
-                <a aria-label={me.profiles[0].network} href={me.profiles[0].network.url}> </a>
-                <a aria-label={me.profiles[1].network} href={me.profiles[1].network.url}> </a>
-                <a aria-label={me.profiles[2].network} href={me.profiles[2].network.url}> </a>
+                <a aria-label={me.profiles[0].network} href={me.profiles[0].network.url}> <BsGithub className='hover:text-fuchsia-700' /> </a>
+                <a aria-label={me.profiles[1].network} href={me.profiles[1].network.url}> <AiFillLinkedin className='hover:text-fuchsia-700' /> </a>
+                <a aria-label={me.profiles[2].network} href={me.profiles[2].network.url}> <CgProfile className='hover:text-fuchsia-700' /></a>
             </div>
         </div >
     );
