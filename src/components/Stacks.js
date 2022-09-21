ae06cb9fb58e0c211cc81d1d1e509aa8d6d2e2f5
Brian Alegre <brialegre@yahoo.com>
2022-09-20 21:24:16 -0700

    Changed styling of Stacks Component to be like Project Component

--- src/components/Stacks.js
+++ src/components/Stacks.js
@@ -12,25 +12,45 @@ function Stacks(resumeData) {
     // Map through skills
     const stacksList = stacks.map((stack, index) => {
         return (
-            <ul key={stack + index}>
-                <li className="space-x-1">
-                    <span className="font-bold">{stack.name}    </span>
-                    {/* Map each keyword from each stack name */}
-                    {stack.keywords.map((keyword, index, arr) => {
-                        if (index === arr.length - 1) {
-                            // Last keyword
-                            return (
-                                <span key={keyword + index} className="text-gray-600">{keyword}</span>
-                            )
-                        } else {
-                            // Not last keyword
-                            return (
-                                <span key={keyword + index} className="text-gray-600">{keyword}, </span>
-                            )
-                        }
-                    })}
-                </li>
-            </ul>
+            // <ul key={stack + index}>
+            //     <li className="space-x-1">
+            //         <span className="font-bold">{stack.name}    </span>
+            //         {/* Map each keyword from each stack name */}
+            //         {stack.keywords.map((keyword, index, arr) => {
+            //             if (index === arr.length - 1) {
+            //                 // Last keyword
+            //                 return (
+            //                     <span key={keyword + index} className="text-gray-600">{keyword}</span>
+            //                 )
+            //             } else {
+            //                 // Not last keyword
+            //                 return (
+            //                     <span key={keyword + index} className="text-gray-600">{keyword}, </span>
+            //                 )
+            //             }
+            //         })}
+            //     </li>
+            // </ul>
+
+            <div key={stack.name + index} className="col-span-2 lg:col-span-1 text-gray-600">
+                <div className="flex items-center space-x-2">
+                    <p className="font-bold text-gray-800">{stack.name}</p>
+
+                </div>
+                {stack.keywords.map((keyword, index, arr) => {
+                    if (index === arr.length - 1) {
+                        // Last keyword
+                        return (
+                            <span key={keyword + index} className="text-gray-600">{keyword}</span>
+                        )
+                    } else {
+                        // Not last keyword
+                        return (
+                            <span key={keyword + index} className="text-gray-600">{keyword}, </span>
+                        )
+                    }
+                })}
+            </div>
         )
     })
 
@@ -42,7 +62,9 @@ function Stacks(resumeData) {
                 </p>
                 <div className="bg-fuchsia-700 absolute bg-opacity-50 w-12 top-4 z-10 h-6 rounded-sm"></div>
             </div>
-            {stacksList}
+            <div className="grid grid-cols-2 gap-5">
+                {stacksList}
+            </div>
         </div>
 
     )
